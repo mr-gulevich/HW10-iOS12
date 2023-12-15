39c88897c088123cb25ad6f41152cf7e3370a792
mr-gulevich <gulevichlesha@gmail.com>
2023-12-15 20:55:25 +0300

    Initial Commit

---
+++ HW10-iOS12/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW10-iOS12
+//
+//  Created by Gulevich Aliaksei on 15.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW10-iOS12/HW10_iOS12App.swift
@@ -0,0 +1,17 @@
+//
+//  HW10_iOS12App.swift
+//  HW10-iOS12
+//
+//  Created by Gulevich Aliaksei on 15.12.23.
+//
+
+import SwiftUI
+
+@main
+struct HW10_iOS12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
